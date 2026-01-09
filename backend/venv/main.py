3a1d9b6643328100535fa2a592925bf071483ad3
Mert Mertcan <mertmcan@gmail.com>
2026-01-09 15:31:29 +0300

    add get/students endpoint

--- backend/venv/main.py
+++ backend/venv/main.py
@@ -4,8 +4,15 @@ from fastapi import FastAPI # from the fastapi package, install the FastAPI clas
 
 app = FastAPI () # create a FastAPI object from the FastAPI class
 
+STUDENTS = [
+    {"id": 1, "name": "Ayse"},
+    {"id": 2, "name": "Memo"},
+]
+
 @app.get("/health")
 def health_check():
-
     return {"status":"ok", "service":"special-ed-platform-backend"}
     
+@app.get("/students")
+def list_students():
+    return STUDENTS
